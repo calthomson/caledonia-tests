a3f60cfb691a9f0b0f975eea754f6890f9c57fa2
calthomson <caledonia.thomson@segment.com>
2021-02-09 18:28:13 -0800

    More tracking

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var Analytics = require('analytics-node');
-var analytics = new Analytics('6EfkfQri8SKBDpCD8KGETFB6RWi3k1yB');
-
-// analytics.track({
-//   userId:'abc',
-//   event: 'Homepage Visited',
-//   properties: {
-//     email: "iheartsf@segment.com",
-//     favorite_color: "green"
-//   }
-// });
-
-analytics.track({
-  userId:'abc',
-  event: 'Site Visited',
-  properties: {
-    name: "secret"
-  }
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
